8b4af868707e0f6ceb34dfa87136e3e1047a4219
nandkumarj <nandkumarj@vmware.com>
2018-02-14 14:41:42 -0800

    add user workflow changes

--- Gen 2/mwc-nfv-hackathon/wizard/src/index.js
+++ Gen 2/mwc-nfv-hackathon/wizard/src/index.js
@@ -1,3 +1,28 @@
+//###########################################################################
+//##
+//# Copyright 2017-2018 VMware Inc.
+//# This file is part of VNF-ONboarding
+//# All Rights Reserved.
+//#
+//# Licensed under the Apache License, Version 2.0 (the "License"); you may
+//# not use this file except in compliance with the License. You may obtain
+//# a copy of the License at
+//#
+//#         http://www.apache.org/licenses/LICENSE-2.0
+//#
+//# Unless required by applicable law or agreed to in writing, software
+//# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//# License for the specific language governing permissions and limitations
+//# under the License.
+//#
+//# For those usages not covered by the Apache License, Version 2.0 please
+//# contact:  osslegalrouting@vmware.com
+// 
+//##
+// 
+//############################################################################
+
 const angular = require('angular');
 require('angular-ui-router');
 
@@ -25,12 +50,14 @@ const epavcsComponent = require('./app/components/epa_configurations_vcosm_compo
 const epaostosmComponent = require('./app/components/epa_configurations_ostosm_component');
 const epavccloudifyComponent = require('./app/components/epa_configurations_vccloudify_component');
 const epaoscloudifyComponent = require('./app/components/epa_configurations_oscloudify_component');
+const signupComponent = require('./app/components/signup_component');
 
 /* Services */
 const dataService = require('./app/services/data_service');
 const navigationService = require('./app/services/navigation_service');
 // const dbService = require('./app/services/db_service');
 const authService = require('./app/services/auth_service');
+const signupService = require('./app/services/signup_service');
 
 /* CSS */
 require('./index.scss');
@@ -44,6 +71,7 @@ module.exports = angular
   .service('navigationService', navigationService)
   // .service('dbService',dbService)
   .service('authService',authService)
+  .service('signupService',signupService)
   .component('wizard', wizardComponent)
   .component('vnf', vnfComponent)
   .component('nic', nicComponent)
@@ -59,5 +87,6 @@ module.exports = angular
   .component('login',loginComponent)
   .component('epavccloudify', epavccloudifyComponent)
   .component('epaoscloudify', epaoscloudifyComponent)
+  .component('signup',signupComponent)
   .name
 ;
