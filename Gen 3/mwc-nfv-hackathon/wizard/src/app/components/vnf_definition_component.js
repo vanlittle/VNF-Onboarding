0a959c45bc668bf12f37d005a404fe1e93e38d5c
amolbjadhav <amolj@vmware.com>
2018-02-21 05:39:30 -0800

    Modified the code for Unit Testing defects

--- Gen 3/mwc-nfv-hackathon/wizard/src/app/components/vnf_definition_component.js
+++ Gen 3/mwc-nfv-hackathon/wizard/src/app/components/vnf_definition_component.js
@@ -36,6 +36,7 @@ module.exports = {
     this.CUSTOM_FLAVOR = "auto"; 
     this.TOSCA_NAME = "TOSCA 1.1"; 
     this.OSM_NAME = 'OSM 3.0';
+    this.RIFT_NAME = 'RIFT.ware 5.3';
     this.DISABLED_FORM_GROUP = 'form-group disabled';
     this.FORM_GROUP = 'form-group';
     this.INPUT_PLACEHOLDER = "Type here";
@@ -54,7 +55,7 @@ module.exports = {
     this.VIMType = ['vCloud Director', 'OpenStack'];
     this.VIMTypeSelected = config.VIMType;
 
-    this.OrchType = ['TOSCA 1.1', 'Cloudify 3.4','Cloudify 4.0', 'OSM 3.0'];
+    this.OrchType = ['TOSCA 1.1', 'RIFT.ware 5.3', 'Cloudify 3.4','Cloudify 4.0', 'OSM 3.0'];
     this.OrchTypeSelected = config.OrchType;
 
     this.vnfDescription = config.VNFDescription || this.VNFTypeSelected;
@@ -101,12 +102,17 @@ module.exports = {
     };
 
     this.isOpenStackOSMClass = function() {
-      return ((this.isOpenStack()) &&(this.OSM()))? this.FORM_GROUP : this.DISABLED_FORM_GROUP;
+      return ((this.isOpenStack()) &&(this.isOSM() || this.isRIFT()))? this.FORM_GROUP : this.DISABLED_FORM_GROUP;
     };
    
     this.isOSM = function() {
       return this.OrchTypeSelected === this.OSM_NAME;
     };
+
+    this.isRIFT = function() {
+      return this.OrchTypeSelected === this.RIFT_NAME;
+    };
+   
    
     this.isOSM_VCDClass = function() {
        //alert(this.FlavorSelected); 
@@ -122,7 +128,7 @@ module.exports = {
     };
     
     this.isOSM_TOSCA_CUSTOM_FLAVOR_Class = function() {
-        if((this.FlavorSelected == "auto") &&(this.isOpenStack()) &&(this.OrchTypeSelected == 'TOSCA 1.1')){
+        if((this.FlavorSelected == "auto") &&(this.isOpenStack()) &&(this.OrchTypeSelected == 'TOSCA 1.1' || this.OrchTypeSelected == 'Cloudify 3.4' || this.OrchTypeSelected == 'Cloudify 4.0')){
 	     return this.FORM_GROUP
         }
         else{
@@ -131,11 +137,11 @@ module.exports = {
     };
     
     this.isOSM_or_VCD_Class = function() {
-        if((this.FlavorSelected == "auto") &&(this.isOpenStack()) &&(this.OrchTypeSelected == 'TOSCA 1.1')){
+        if((this.FlavorSelected == "auto") &&(this.isOpenStack()) &&(this.OrchTypeSelected == 'TOSCA 1.1' || this.OrchTypeSelected == 'Cloudify 3.4' || this.OrchTypeSelected == 'Cloudify 4.0')){
 	     return this.FORM_GROUP
         }
         else{
-           return ((this.isOSM())|| (this.isVCD())) ? this.FORM_GROUP : this.DISABLED_FORM_GROUP;
+           return ((this.isOSM())|| (this.isRIFT()) || (this.isVCD())) ? this.FORM_GROUP : this.DISABLED_FORM_GROUP;
         }
     };
     this.isCUSTOM_FLAVOR = function() {
